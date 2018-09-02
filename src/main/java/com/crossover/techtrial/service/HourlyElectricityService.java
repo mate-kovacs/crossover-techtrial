9d96ed30c1206fde75ab5ae755b3b4662ecf13f0
Kovács Máté <kismate90@gmail.com>
2018-09-02 22:15:08 +0200

    Added abstract method to get daily electricities by panel.

--- src/main/java/com/crossover/techtrial/service/HourlyElectricityService.java
+++ src/main/java/com/crossover/techtrial/service/HourlyElectricityService.java
@@ -1,8 +1,11 @@
 package com.crossover.techtrial.service;
 
+import com.crossover.techtrial.dto.DailyElectricity;
 import com.crossover.techtrial.model.HourlyElectricity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 /**
  * HourlyElectricityService interface for all services realted to HourlyElectricity.
@@ -13,4 +16,6 @@ public interface HourlyElectricityService {
   HourlyElectricity save(HourlyElectricity hourlyElectricity);
   
   Page<HourlyElectricity> getAllHourlyElectricityByPanelId(Long panelId, Pageable pageable);
+
+  List<DailyElectricity> getDailyElectriciryByPanelId(Long panelId);
 }
